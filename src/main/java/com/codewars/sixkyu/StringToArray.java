94d9fa2affa8c55df37e72c19e8382442b1c426b
MagdaSzut <magda.szut@gmail.com>
2022-09-30 22:17:55 +0200

    split method + test - final

---
+++ src/main/java/com/codewars/sixkyu/StringToArray.java
@@ -0,0 +1,8 @@
+package com.codewars.sixkyu;
+/*Write a function to split a string and convert it into an array of words.*/
+
+public class StringToArray {
+    public static String[] stringToArray(String s) {
+        return s.split(" ");
+    }
+}
---
+++ src/test/java/com/codewars/sixkyu/StringToArrayTest.java
@@ -0,0 +1,13 @@
+package com.codewars.sixkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class StringToArrayTest {
+    @Test
+    public void basicTests() {
+        assertArrayEquals(new String[]{"Robin", "Singh"}, StringToArray.stringToArray("Robin Singh"));
+        assertArrayEquals(new String[]{"I", "love", "arrays", "they", "are", "my", "favorite"}, StringToArray.stringToArray("I love arrays they are my favorite"));
+    }
+}
