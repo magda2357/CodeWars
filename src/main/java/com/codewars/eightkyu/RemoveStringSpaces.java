b91f4bb18730587917c9a395da437ed0897e9b58
MagdaSzut <magda.szut@gmail.com>
2022-10-01 20:06:03 +0200

    replace method + test - final

---
+++ src/main/java/com/codewars/eightkyu/RemoveStringSpaces.java
@@ -0,0 +1,9 @@
+package com.codewars.eightkyu;
+
+/*Simple, remove the spaces from the string, then return the resultant string.*/
+public class RemoveStringSpaces {
+
+    public static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+}
