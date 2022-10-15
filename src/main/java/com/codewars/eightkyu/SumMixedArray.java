7fe1bdd2f0c8f758ca8c59d627fab2975a472677
MagdaSzut <magda.szut@gmail.com>
2022-10-15 10:56:24 +0200

    first commit

---
+++ src/main/java/com/codewars/eightkyu/SumMixedArray.java
@@ -0,0 +1,18 @@
+//package com.codewars.eightkyu;
+//    /*Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+//Return your answer as a number.*/
+///*
+// * Assume input will be only of Integer o String type
+// */
+//
+//import java.io.LineNumberInputStream;
+//import java.util.List;
+//
+//public class SumMixedArray {
+//    public int sum(List<?> mixed) {
+//        for ( a : mixed) {
+//
+//        }
+//        return -1;
+//    }
+//}
