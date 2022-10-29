fd7285ebc1c8649b28312a7bb0a804dfde63ab70
MagdaSzut <magda.szut@gmail.com>
2022-10-29 11:01:34 +0200

    second version, but with  tests

--- src/main/java/com/codewars/sixkyu/SortTheOdd.java
+++ src/main/java/com/codewars/sixkyu/SortTheOdd.java
@@ -2,15 +2,14 @@ package com.codewars.sixkyu;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 /*You will be given an array of numbers.
 You have to sort the odd numbers in ascending order while leaving
 the even numbers at their original positions.*/
 public class SortTheOdd {
     public static int[] sortArray(int[] array) {
-        Set<Integer> sortedlist = new TreeSet<>();
+        List<Integer> sortedlist = new ArrayList<>();
         List<Integer> index = new ArrayList<>();
 
         for (int i = 0; i < array.length - 1; i++) {
@@ -19,11 +18,14 @@ public class SortTheOdd {
                 index.add(i);
             }
         }
-        List<Integer> listNumbers = new ArrayList<>(sortedlist);
+        sortedlist = sortedlist.stream().
+                sorted()
+                .collect(Collectors.toList());
 
-        for (int i = 0; i < listNumbers.size(); i++) {
-            array[index.get(i)] = listNumbers.get(i);
+        for (int i = 0; i < sortedlist.size(); i++) {
+            array[index.get(i)] = sortedlist.get(i);
         }
+
         return array;
     }
 }
