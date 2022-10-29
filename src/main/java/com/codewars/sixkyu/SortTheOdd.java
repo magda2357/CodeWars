6e6a1e4ab6a258c4d13dac65ffc2cf4546c1de3b
MagdaSzut <magda.szut@gmail.com>
2022-10-29 13:24:44 +0200

    third version - correction of table size & clever version from code wars

--- src/main/java/com/codewars/sixkyu/SortTheOdd.java
+++ src/main/java/com/codewars/sixkyu/SortTheOdd.java
@@ -1,6 +1,7 @@
 package com.codewars.sixkyu;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,4 +29,33 @@ public class SortTheOdd {
 
         return array;
     }
+
+    public static void main(String[] args) {
+        int[] array={50, 88, 24, 76, 30, 52, 7, 88, 25, 44, 49, 70, 7, 0, 35, 92, 65, 87, 48, 2, 17};
+        List<Integer> sortedlist = new ArrayList<>();
+        List<Integer> index = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                sortedlist.add(array[i]);
+                index.add(i);
+            }
+        }
+        sortedlist = sortedlist.stream().
+                sorted()
+                .collect(Collectors.toList());
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < sortedlist.size(); i++) {
+            array[index.get(i)] = sortedlist.get(i);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    /*CLEVER FROM CODE WARS
+        // Sort the odd numbers only
+    final int[] sortedOdd = Arrays.stream(array).filter(e -> e % 2 == 1).sorted().toArray();
+
+    // Then merge them back into original array
+    for (int j = 0, s = 0; j < array.length; j++) {
+      if (array[j] % 2 == 1) array[j] = sortedOdd[s++];
+    }*/
 }
