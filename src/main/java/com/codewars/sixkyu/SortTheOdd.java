1ba3c490a675f8195286490c67dedc0699f9f3b5
MagdaSzut <magda.szut@gmail.com>
2022-10-29 10:39:48 +0200

    second version, still tree set, but transfer to array/not list

--- src/main/java/com/codewars/sixkyu/SortTheOdd.java
+++ src/main/java/com/codewars/sixkyu/SortTheOdd.java
@@ -1,41 +1,29 @@
 package com.codewars.sixkyu;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /*You will be given an array of numbers.
 You have to sort the odd numbers in ascending order while leaving
 the even numbers at their original positions.*/
 public class SortTheOdd {
     public static int[] sortArray(int[] array) {
-        return array;
-    }
-
-    public static void main(String[] args) {
-        int[] array = {9, 7, 8, 6, 5, 4, 3, 2, 1, 0};
-
-        List<Integer> list = Arrays.stream(array)
-                .boxed()
-                .collect(Collectors.toList());
-
-        List<Integer> index = new ArrayList<>();
         Set<Integer> sortedlist = new TreeSet<>();
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (list.get(i) % 2 == 1) {
-                sortedlist.add(list.get(i));
+        List<Integer> index = new ArrayList<>();
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] % 2 == 1) {
+                sortedlist.add(array[i]);
                 index.add(i);
             }
         }
-        List<Integer> listNumbers = new ArrayList<Integer>(sortedlist);
-//        List<Integer> listNumbers = list.stream().
-//                filter(a -> a % 2 == 1)
-//                .sorted()
-//                .collect(Collectors.toList());
+        List<Integer> listNumbers = new ArrayList<>(sortedlist);
 
-        System.out.println(list);
         for (int i = 0; i < listNumbers.size(); i++) {
-            list.set(index.get(i), listNumbers.get(i));
+            array[index.get(i)] = listNumbers.get(i);
         }
-        System.out.println(list);
+        return array;
     }
 }
