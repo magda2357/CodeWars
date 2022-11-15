e4103cd7b4e0a64e796c2c09dcdb80fa73ee5fe3
MagdaSzut <magda.szut@gmail.com>
2022-11-15 12:59:41 +0100

    simpler version with String split & join, Collections.reverseOrder

--- src/main/java/com/codewars/sevenkyu/DescendingOrder.java
+++ src/main/java/com/codewars/sevenkyu/DescendingOrder.java
@@ -1,12 +1,13 @@
 package com.codewars.sevenkyu;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class DescendingOrder {
     public static int sortDesc(final int num) {
-        char[] digits = String.valueOf(num).toCharArray();
-        Arrays.sort(digits);
-        return Integer.parseInt(String.valueOf(new StringBuilder(String.valueOf(digits)).reverse()));
+        String[] digits = String.valueOf(num).split("");
+        Arrays.sort(digits, Collections.reverseOrder());
+        return Integer.parseInt(String.join("", digits));
     }
 }
 
